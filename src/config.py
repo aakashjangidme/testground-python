747084e1b804c686999a002159b2e45236bd25f8
Aakash Jangid <ajangid25@gmail.com>
2021-02-27 19:35:20 +0530

    test-env rds setup done w/ windows

--- src/config.py
+++ src/config.py
@@ -6,19 +6,31 @@ gen_err = "something went wrong."
 
 import logging, os
 
-DEBUG = os.getenv('ENVIRONMENT') == 'DEV'
+DEBUG = os.getenv('ENVIRONMENT') == 'TEST'
 HOST = os.getenv('APP_HOST')
 PORT = int(os.getenv('APP_PORT', 3000))
 
 
-DB_CONFIG = {
-    "host": os.getenv('DB_HOST', 'localhost'),
-    "port": int(os.getenv('DB_PORT', 3300)),
-    "user": os.getenv('DB_USER'),
-    "pass":  os.getenv('DB_PASS'),
-    "db": os.getenv('DB'),
-    "charset": 'utf8'
-}
+
+if DEBUG:
+    DB_CONFIG = {
+        "host": os.getenv('DB_HOST', 'localhost'),
+        "port": int(os.getenv('DB_PORT', 3300)),
+        "user": os.getenv('DB_USER'),
+        "pass":  os.getenv('DB_PASS'),
+        "db": os.getenv('DB'),
+        "charset": 'utf8'
+    } 
+else :
+    DB_CONFIG = {
+        "host": os.getenv('RDS_HOST'),
+        "port": int(os.getenv('RDS_PORT', 3300)),
+        "user": os.getenv('RDS_USER'),
+        "pass":  os.getenv('RDS_PASS'),
+        "db": os.getenv('RDS_DB'),
+        "charset": 'utf8'
+    } 
+      
 
 DB_URI = None
 
